3b925e2b5c5eac2013b5d95ca08b36b245b61ece
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 17:38:45 -0400

    Refactor "findWaldo" function to use "forEach()" method instead of for loop

--- findWaldo.js
+++ findWaldo.js
@@ -1,15 +1,16 @@
 // Copied from Compass "Functions As Objects" Assignment
 // The second argument/parameter is expected to be a (callback) function
-const findWaldo = function (names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
+const findWaldo = function(names, found) {
+  names.forEach(function(name, index) { // Refactor function to use forEach() method instead of for loop below
+  //for (let i = 0; i < names.length; i++) {
+    //let name = names[i];
     if (name === "Waldo") {
-      found(i); // execute callback with index
+      found(index); // execute callback with index
     }
-  }
+  });
 };
 
-const actionWhenFound = function (index) {
+const actionWhenFound = function(index) {
   console.log(`Found Waldo at index ${index}`);
 };
 
